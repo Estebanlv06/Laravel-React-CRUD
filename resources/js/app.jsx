5c43534fa54ab50891a23c6efcd8e25dbbe954a6
Estebanlv06 <estebanlv06@gmail.com>
2023-06-12 22:34:10 -0400

    Prueba de un commit

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -15,7 +15,7 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 const appName = window.document.getElementsByTagName('title')[0]?.innerText;
 
 createInertiaApp({
-    title: (title) => `${title}`,
+    title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
     setup({ el, App, props }) {
         const root = createRoot(el);
